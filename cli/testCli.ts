4de8468b10ae2658f5b41dba9183a3eed5268df6
Ubuntu <user@ubuntu.com>
2022-04-02 04:45:11 -0700

    clean up

--- cli/testCli.ts
+++ cli/testCli.ts
@@ -1,6 +1,8 @@
 import { Contract, ethers } from "ethers";
-import { erc42069 } from "./abi/ERC42069";
+import { ERC42069_ABI } from "./abi/ERC42069_ABI";
 import "dotenv/config";
+
+/// ============ ETHERS PROVIDER SETUP ============
 
 // Network to connect to
 const network = "arbitrum-rinkeby";
@@ -8,14 +10,23 @@ const network = "arbitrum-rinkeby";
 // Read from .env
 const api_key = process.env.ALCHEMY_API_KEY;
 
-// Contract Addresses
-const ERC42069_ADDR = '0x29Bd3a45560E7D13Aa95aFC8cC68676454Eb75b0';
-
 const provider = new ethers.providers.AlchemyProvider(network,
     api_key
 );
 
-const contract = new ethers.Contract(ERC42069_ADDR, erc42069, provider);
+/// ============ CONTRACT SETUP ============
+
+/// TODO: Setup contract factory
+
+// Contract Address
+const ERC42069_ADDR = '0x29Bd3a45560E7D13Aa95aFC8cC68676454Eb75b0';
+
+// The actual setup
+const contract = new ethers.Contract(ERC42069_ADDR, ERC42069_ABI, provider);
+
+/// ============ ASYNC FUNCTION CALLS ============
+
+// here to make sure I didn't break shit
 async function bro() {
     console.log(
         await contract.name() + " | ",
