b8504529ee252f05e52a930e2736385d2b73ff3e
abran <82933273+abrandec@users.noreply.github.com>
2022-04-10 00:47:52 -0700

    Update and rename ERC42069.t.sol to ERC721Vault.t.sol

--- src/test/ERC721Vault.t.sol
+++ src/test/ERC721Vault.t.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.12;
 
-import {ERC42069} from "../ERC42069.sol";
+import {ERC721Vault} from "../ERC721Vault.sol";
 import {MockERC721} from "solmate/test/utils/mocks/MockERC721.sol";
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
 
 // Prettier is crapping out on test files
-contract ERC42069Test is DSTestPlus {
-    ERC42069 erc42069;
+contract ERC721VaultTest is DSTestPlus {
+    ERC721Vault erc721Vault;
     MockERC721 mockERC721;
 
     function setUp() public {
-        console.log(unicode"🧪 Testing ERC420721.sol...");
+        console.log(unicode"🧪 Testing ERC721Vault.sol...");
         mockERC721 = new MockERC721("TEST", "T_T");
-        erc42069 = new ERC42069(mockERC721, "TEST_COIN", "TC", 18);
+        erc721Vault = new ERC721Vault(mockERC721, "TEST_COIN", "TC", 18);
     }
 
     // the batch mintoor
@@ -30,17 +30,17 @@ contract ERC42069Test is DSTestPlus {
         index[0] = 0;
         mintERC721(index);
         vm.startPrank(address(0xBEEF));
-        mockERC721.approve(address(erc42069), 0);
+        mockERC721.approve(address(erc721Vault), 0);
         uint256[] memory id = new uint256[](1);
         id[0] = 0;
-        erc42069.mint(id, address(0xBEEF));
+        erc721Vault.mint(id, address(0xBEEF));
     }
 
     function testWithdraw() public {
         testDeposit();
         uint256[] memory id = new uint256[](1);
         id[0] = 0;
-        erc42069.withdraw(id, address(0xBEEF));
+        erc721Vault.withdraw(id, address(0xBEEF));
     }
 
     function testDepositDiffUser() public {
@@ -49,14 +49,14 @@ contract ERC42069Test is DSTestPlus {
         uint256[] memory id = new uint256[](1);
         id[0] = 0;
         vm.expectRevert(bytes("NOT_AUTHORIZED"));
-        mockERC721.approve(address(erc42069), 0);
+        mockERC721.approve(address(erc721Vault), 0);
         vm.expectRevert(bytes("WRONG_FROM"));
-        erc42069.mint(id, address(0xBEEF));
+        erc721Vault.mint(id, address(0xBEEF));
     }
 
     function testTransfer() public {
         testDeposit();
-        erc42069.transfer(address(0), 1 wei);
+        erc721Vault.transfer(address(0), 1 wei);
     }
 
     /// ============ TESTS INTENDED TO FAIL ============
@@ -75,10 +75,10 @@ contract ERC42069Test is DSTestPlus {
     // Results in Arithmetic over/underflow
     function testWithdrawNotEnough() public {
         testDeposit();
-        erc42069.transfer(address(0), 1 wei);
+        erc721Vault.transfer(address(0), 1 wei);
         uint256[] memory id = new uint256[](1);
         id[0] = 0;
-        erc42069.withdraw(id, address(0xBEEF));
+        erc721Vault.withdraw(id, address(0xBEEF));
     }
 
     function testDepositReentrancy() public {
